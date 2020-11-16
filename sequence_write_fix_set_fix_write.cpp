9c9dbcbf6aa0544da07f5d60353e2d1347f870d5
1552980358 <1552980358@qq.com>
2020-11-16 18:34:19 +0800

    Complete sequence_write_fix_set_fix_write
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- sequence_write_fix_set_fix_write.cpp
+++ sequence_write_fix_set_fix_write.cpp
@@ -3,3 +3,79 @@
 //
 
 #include "sequence_write_fix_set_fix_write.h"
+
+void sequence_write_fix_set_fix_write(const int type, string *target_path, int *no_of_set, int *no_of_gb) {
+
+    cout << "Generate data..........";
+
+    byte data[*no_of_set];
+    switch (type) {
+        case 0:
+            for (auto &i: data) {
+                i = 0;
+            }
+            break;
+        case 1:
+            for (auto &i: data) {
+                i = 1;
+            }
+        case 2:
+            for (auto &i: data) {
+                // 10101010
+                i = 252;
+            }
+            break;
+        default:
+            return;
+    }
+
+    cout << "Succeed" << endl
+         << "Opening temporary test file..........";
+    FILE *file = fopen(target_path->c_str(), "wb+");
+    if (file == nullptr) {
+        cout << "Failed" << endl;
+        return;
+    }
+    cout << "Succeed" << endl
+         << "Launching thread..........";
+
+    InformationListener listener;
+    pthread_t pthread;
+    pthread_create(&pthread, nullptr, sub_thread, (void *) &listener);
+    int i;
+    for (i = 1; i <= 10 && !listener.thread_signal; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            cout << '\b';
+        }
+        cout << i << "s";
+        sleep_in_millisecond(1000);
+    }
+
+    for (int j = 0; j < (i >= 10 ? 3 : 2); ++j) {
+        cout << '\b';
+    }
+    if (!listener.thread_signal) {
+        cout << "Failed" << endl;
+        return;
+    }
+    cout << "Succeed" << endl
+         << "Test launch:" << endl;
+
+    listener.thread_state = 1;
+    for (i = 0; i < *no_of_gb * MB; ++i) {
+        fwrite(data, KB, 1, file);
+        listener.wrote_size += KB;
+    }
+    fclose(file);
+
+    cout << "..........Completed" << endl
+         << "Remove temporary file.........." << (!remove(target_path->c_str()) ? "Succeed" : "Failed") << endl
+         << endl
+         << "========================================" << endl
+         << "Write Size: " << *no_of_gb << " GB" << endl
+         << "Average: " << cal_size(listener.sample_sum / listener.sample_count) << "/s" << endl
+         << "========================================" << endl
+         << endl
+         << endl;
+
+}
--- sequence_write_fix_set_fix_write.h
+++ sequence_write_fix_set_fix_write.h
@@ -5,4 +5,12 @@
 #ifndef DISKSPEEDTEST_SEQUENCE_WRITE_FIX_SET_FIX_WRITE_H
 #define DISKSPEEDTEST_SEQUENCE_WRITE_FIX_SET_FIX_WRITE_H
 
+#include <iostream>
+#include "cal_size.h"
+#include "sub_thread.h"
+#include "byte_size.h"
+using namespace std;
+
+void sequence_write_fix_set_fix_write(int, string *, int *, int *);
+
 #endif //DISKSPEEDTEST_SEQUENCE_WRITE_FIX_SET_FIX_WRITE_H
